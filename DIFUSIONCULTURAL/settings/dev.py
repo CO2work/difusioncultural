4c329c3413f53385de242679c0bb9bc415d3ccca
lm <luismguzmanz@gmail.com>
2021-06-15 01:06:22 -0500

    Initial commit

---
+++ DIFUSIONCULTURAL/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-t=w^6mp%b$78==kfv2y3(^qea!hy^aj&&54mivf#+dk8pzuopq'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
